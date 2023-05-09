92042294f05e0d1647b5da44f4571ea0784ad6bd
Xero kun <elo48858@gmail.com>
2023-05-09 15:12:14 +0100

    `🌱UPDATE` Added Some Variables

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -1,5 +1,16 @@
 public class MyApplication {
     public static void main(String[] args) {
-        System.out.println("Java is Awesome");
+        int age = 16;
+        String name = "xero";
+        String programmingLanguage = "Java";
+
+        // PRINT SOME OF THESE VARIABLES
+        System.out.println(name + " is currently " + age + " years old.");
+
+        // PRINT SOME MORE THINGS
+        System.out.println("I'm writing " + programmingLanguage + " code.");
+
+        // PRINT THIS TO THE STANDARD OUTPUT (DEFAULTS TO THE TERMINAL)
+        System.out.println("Java is Awesome.");
     }
 }
